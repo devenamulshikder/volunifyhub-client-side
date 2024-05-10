ad380bce9727a6df95ac4840e6733a47dfcc85cb
devenamulshikder <devenamulshikder@gmail.com>
2024-05-10 11:03:27 +0600

    log out functionality added and user update profile show and name show

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,12 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import logo from "../assets/assignment1102.png";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
+import toast from "react-hot-toast";
 
 const NavBar = () => {
+  const { user, logOut } = useContext(AuthContext);
+
   const [theme, setTheme] = useState("light");
   useEffect(() => {
     localStorage.setItem("theme", theme);
@@ -15,6 +19,13 @@ const NavBar = () => {
     } else {
       setTheme("light");
     }
+  };
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {
+        toast.success("Logout Successful");
+      })
+      .catch((err) => toast.error(err?.message));
   };
   return (
     <div>
@@ -32,51 +43,57 @@ const NavBar = () => {
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <div>Home</div>
+              <Link to="/">Home</Link>
             </li>
-
             <li>
               <div>Need Volunteer Page</div>
             </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
+            {!user && (
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            )}
           </ul>
 
-          <div className="dropdown dropdown-end z-50">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full" title="">
-                <img
-                  referrerPolicy="no-referrer"
-                  alt="User Profile Photo"
-                  src=""
-                />
+          {user && (
+            <div className="dropdown dropdown-end z-50">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div
+                  title={user?.displayName}
+                  className="w-10 rounded-full"
+                >
+                  <img
+                    referrerPolicy="no-referrer"
+                    alt="User Profile Photo"
+                    src={user?.photoURL}
+                  />
+                </div>
               </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <div>Add Volunteer Post</div>
+                </li>
+                <li>
+                  <div>Manage My Post</div>
+                </li>
+                <li>
+                  <div>My Volunteer Requested Post</div>
+                </li>
+                <li className="mt-2">
+                  <button onClick={handleLogOut} className="bg-gray-200 block text-center">
+                    Logout
+                  </button>
+                </li>
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <div>Add Volunteer Post</div>
-              </li>
-              <li>
-                <div>Manage My Post</div>
-              </li>
-              <li>
-                <div>My Volunteer Requested Post</div>
-              </li>
-              <li className="mt-2">
-                <button className="bg-gray-200 block text-center">
-                  Logout
-                </button>
-              </li>
-            </ul>
-          </div>
+          )}
           <input
             onChange={handleToggle}
             type="checkbox"
